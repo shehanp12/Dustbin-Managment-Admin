d88bcac58ca721fdde11bd55944a0e85d7066094
shehanp12 <shehanp068@gmail.com>
2020-11-14 16:12:00 +0530

    Responsive Drawer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { Button } from '@material-ui/core';
+import NavBar from "./Components/NavBars/NavBar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <NavBar/>
+
   );
+
+
 }
 
 export default App;
