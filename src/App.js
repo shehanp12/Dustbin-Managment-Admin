ead18c4b7bc2a2e124b179e99c1cc548b0f2ed0c
shehanp12 <shehanp068@gmail.com>
2020-11-14 19:19:33 +0530

    Loading screen

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import React from 'react';
 import { Button } from '@material-ui/core';
 import NavBar from "./Components/NavBars/NavBar";
+import LoadingScreen from "./Components/Loading/Loading";
 function App() {
   return (
-      <NavBar/>
+      <LoadingScreen/>
 
   );
 
