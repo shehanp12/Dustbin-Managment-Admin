16ca0eba2a6386cc5ec97aaa621824269a93a8da
shehanp12 <shehanp068@gmail.com>
2020-11-17 03:33:25 +0530

    unused impoarts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { Button } from '@material-ui/core';
 import NavBar from "./Components/NavBars/NavBar";
-import LoadingScreen from "./Components/Loading/Loading";
+
 function App() {
   return (
       <NavBar/>
--- src/Layouts/Home/Home.js
+++ src/Layouts/Home/Home.js
@@ -1,13 +1,5 @@
 import React,{Component} from "react";
-import IconButton from "@material-ui/core/IconButton";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import {createStyles} from "@material-ui/core";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import ButtonAppBar from "../../Components/AppBar/AppBar";
-
-
-
 
 class Home extends Component{
     render() {
