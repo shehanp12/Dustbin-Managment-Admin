36b80cd5e2dd0aed8b95eaac978dbb4436a53778
shehanp12 <shehanp068@gmail.com>
2020-11-17 02:36:22 +0530

    Route with material ui

---
+++ src/Components/Map/GoogleMaps.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+class GoogleMaps extends Component{
+    render() {
+        return(
+            'Map'
+        )
+    }
+
+}
+
+export default GoogleMaps
--- src/Components/NavBars/NavBar.js
+++ src/Components/NavBars/NavBar.js
@@ -1,71 +1,185 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core/styles"
-
-import {
-    BrowserRouter as Router,
-    Switch, Route, Link
-} from "react-router-dom";
-
-import {
-    Drawer, List, ListItem,
-    ListItemIcon, ListItemText,
-    Container, Typography,
-} from "@material-ui/core";
-
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
 import HomeIcon from "@material-ui/icons/Home";
-import InfoIcon from '@material-ui/icons/Info';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "../Home/Home";
+import GoogleMaps from "../Map/GoogleMaps";
 
-const useStyles = makeStyles((theme) => ({
-    drawerPaper: { width: 'inherit' },
-    link: {
-        textDecoration: 'none',
-        color: theme.palette.text.primary
-    }
-}))
+     const  drawerWidth = 240;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }),
+);
 
-function NavBar() {
-    const classes = useStyles();
-    return (
-        <Router>
-            <div style={{ display: 'flex' }}>
-                <Drawer
-                    style={{ width: '220px' }}
-                    variant="persistent"
-                    anchor="left"
-                    open={true}
-                    classes={{ paper: classes.drawerPaper }}
-                >
-                    <List>
-                        <Link to="/" className={classes.link} >
-                            <ListItem button>
-                                <ListItemIcon>
-                                    <HomeIcon />
-                                </ListItemIcon>
-                                <ListItemText primary={"Home"} />
-                            </ListItem>
-                        </Link>
-                        <Link to="/map" className={classes.link}>
-                            <ListItem button>
-                                <ListItemIcon>
-                                    <InfoIcon />
-                                </ListItemIcon>
-                                <ListItemText primary={"Map"} />
-                            </ListItem>
-                        </Link>
-                    </List>
-                </Drawer>
-                <Switch>
-                    <Route exact path="/" component={Home}>
-
-                    </Route>
-                    <Route exact path="/map" component={Map}>
-
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
 }
 
-export default NavBar;
+export default function ResponsiveDrawer(props: Props) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+
+      <Router>
+          <List>
+              <Link to="/">
+                  <ListItem button>
+                      <ListItemIcon>
+                          <MenuIcon/>
+                      </ListItemIcon>
+                  </ListItem>
+              </Link>
+              <Link to="/map">
+                  <ListItem button>
+                      <ListItemIcon>
+                                <HomeIcon/>
+                      </ListItemIcon>
+                  </ListItem>
+              </Link>
+
+
+          </List>
+
+
+
+
+
+
+
+
+      </Router>
+    </div>
+
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Router>
+
+
+                  <Route>
+                      <Switch>
+                          <Route exact path="/" component={Home}>
+
+                          </Route>
+
+                      </Switch>
+
+                  </Route>
+        </Router>
+
+      </main>
+    </div>
+  );
+}
