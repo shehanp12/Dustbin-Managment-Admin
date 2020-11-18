68182e6999e636082451ae7c246c69f2c2f3325f
shehanp12 <shehanp068@gmail.com>
2020-11-18 23:04:35 +0530

    Smart Bins details

--- src/Layouts/Home/Home.js
+++ src/Layouts/Home/Home.js
@@ -4,6 +4,7 @@ import SpacingGrid from "../../Components/Grid/GridLayout";
 import GridLayout from "../../Components/Grid/GridLayout";
 import styles from  "../layouts.module.css"
 import useFirestore  from '../../Hooks/useFirestore'
+import SimpleCard from "../../Components/Grid/GridLayout";
 
 
 const Home = () =>{
@@ -11,9 +12,13 @@ const Home = () =>{
     console.log(docs)
 
     return (
-        <div>
-            hello
-        </div>
+        <>
+       <ButtonAppBar/>
+       <div className={styles.center}>
+           <SimpleCard/>
+       </div>
+
+       </>
     )
 
 }
