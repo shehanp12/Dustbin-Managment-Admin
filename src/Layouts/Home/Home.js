d2f431e0239847867d5c02efb3ec9042bcb412ea
shehanp12 <shehanp068@gmail.com>
2020-11-20 15:52:36 +0530

    remove unused imports

--- src/Layouts/Home/Home.js
+++ src/Layouts/Home/Home.js
@@ -1,35 +1,21 @@
 import React from "react";
-import ButtonAppBar from "../../Components/AppBar/AppBar";
-import styles from  "../layouts.module.css"
-import useFirestore  from '../../Hooks/useFirestore'
 import DriverDetails from "../../Components/Grid/DriverDetails";
+import SmartDetails from "../../Components/Grid/SmartDetails";
+import {AppBar} from "@material-ui/core";
 
 
 
 const Home = () =>{
-    const {docs} = useFirestore('Bins');
-    console.log(docs)
+
     return (
 
+         <>
+             <AppBar/>
+        <DriverDetails/>
 
-<>
+        <SmartDetails/>
+        </>
 
-    <DriverDetails/>
-
-    <div>
-        <h1>Smart Bin Details</h1>
-    </div>
-           { docs.map(doc => {
-                return(
-                    <div >
-
-                     <p >Bin Id</p>
-                        <p>{doc.BinId}</p>
-                    </div>
-                )})}
-
-
-                </>
 
 
 
