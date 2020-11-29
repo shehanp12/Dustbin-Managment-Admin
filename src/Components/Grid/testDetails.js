4ad60aaca5f76ca6a7d3264a94407158cfa425a3
shehanp12 <shehanp068@gmail.com>
2020-11-30 04:19:53 +0530

    pass the map

--- src/Components/Grid/testDetails.js
+++ src/Components/Grid/testDetails.js
@@ -64,7 +64,7 @@ export default function StickyHeadTable() {
     const classes = useStyles();
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
-    const {docs} = useFirestore('Drivers');
+
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -74,8 +74,10 @@ export default function StickyHeadTable() {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
-
+    const {docs} = useFirestore('Drivers');
+     docs.map()
     return (
+
         <Paper className={classes.root}>
             <TableContainer className={classes.container}>
                 <Table stickyHeader aria-label="sticky table">
@@ -93,20 +95,14 @@ export default function StickyHeadTable() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                            return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                    {columns.map((column) => {
-                                        const value = row[column.id];
-                                        return (
-                                            <TableCell key={column.id} align={column.align}>
-                                                {column.format && typeof value === 'number' ? column.format(value) : value}
-                                            </TableCell>
-                                        );
-                                    })}
-                                </TableRow>
-                            );
-                        })}
+
+                        docs.map(doc => {
+
+
+
+
+                    })
+                        }
                     </TableBody>
                 </Table>
             </TableContainer>
